eb0497c84470b5409baf6a87a2e6cf691e6ed298
Isadora Sophia <saecia@gmail.com>
2017-06-04 23:35:02 -0300

    First try for our main program.

--- dijkstra/findpath.hs
+++ dijkstra/findpath.hs
@@ -1,4 +1,19 @@
-main = do
-    contents <- getContents
-    putStrLn contents
 
+-- data structures for our graph
+data Node = String [Path] deriving (Show, Read, Eq)
+data Path = Path { to :: Node, from :: Node, weight :: Float } deriving (Show, Read, Eq)
+
+type Graph = [Node]
+
+-- main loop
+main = do
+        content <- getContents
+        let paths = [ x | x <- content, not $ isgarbage x ]
+            source = last $ init paths
+            target = last paths
+        putStrLn $ show source
+        putStrLn $ show target
+        putStrLn $ show paths
+
+-- check if a given character is useful
+isgarbage x = if x == '\n' || x == ' ' then True else False
