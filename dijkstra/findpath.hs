081f1385675efba17fc87632103d7f6e9f13f06b
Matheus Mortatti <matheusmortatti@gmail.com>
2017-06-08 14:57:22 -0300

    Dijkstra algorithm - Need to sort the answer properly

--- dijkstra/findpath.hs
+++ dijkstra/findpath.hs
@@ -14,6 +14,12 @@ data Edge = Edge { from :: String, to :: String, weight :: Float } deriving (Sho
 data Node = Node { key :: String, edges :: [Edge] } deriving (Show, Read, Eq)
 type Graph = [Node]
 
+data Cost = Cost { idf :: String, cost :: Float, bgraph :: Bool} deriving (Show, Read, Eq)
+data Parent = Parent { child :: String, parent :: String } deriving (Show, Read, Eq)
+
+type Costs = [Cost]
+type Parents = [Parent]
+
 main = do
         input <- getContents
         let content = lines input
@@ -73,3 +79,65 @@ deleteEdge _ [] = []
 deleteEdge target (h:r) = if h == target then r else h:deleteEdge target r
 
 -- WAKE ME UP INSIDEEEEEEEEEEEE
+
+-- retrieve id of minimal cost node
+getMinNode :: Costs -> String
+getMinNode costs = snd ( foldl (\ (min, string) c -> if and [or [(min > cost c), (min == -1)], bgraph c] then (cost c, idf c) else (min, string)) (-1, []) costs )
+
+-- add new parent to the list if it doesn´t exists and replace if it exists
+addParent :: String -> String -> Parents -> Parents
+addParent c p [] = [(Parent c p)]
+addParent c p ((Parent child parent):r) = if child == c then ((Parent c p):r)
+                                          else ((Parent child parent):(addParent c p r))
+
+updateCost :: String -> Float -> Costs -> Costs
+updateCost i nc [] = [(Cost i nc True)]
+updateCost i nc (c:r) =  if idf c == i then 
+                                        if cost c > nc then ((Cost i nc (bgraph c)):r)
+                                        else (c:r)
+                                    else
+                                        (c:(updateCost i nc r))
+
+findCost :: String -> Costs -> Float
+findCost _ [] = -1
+findCost s (c:r) = if idf c == s then cost c
+                                else findCost s r
+retrieveNode :: String -> Graph -> Node
+retrieveNode target [] = (Node target [])
+retrieveNode target (h:r) = if key h == target then h else retrieveNode target r
+
+removeNode :: Node -> Graph -> Graph
+removeNode _ [] = []
+removeNode (Node n i) ((Node k e):r) = if n == k then r else ((Node k e):(removeNode (Node n i) r))
+
+dijkstra :: Graph -> (Parents, Costs) -> (Parents, Costs)
+dijkstra [] (parents, costs) = (parents, costs)
+dijkstra graph (parents, costs) = 
+                    dijkstra (removeNode (minNode) graph) (iterateEdges minNode parents (setBoolCost minCost False costs))
+                    where 
+                        minCost = (getMinNode costs)
+                        minNode = retrieveNode minCost graph
+
+
+iterateEdges :: Node -> Parents -> Costs -> (Parents, Costs)
+iterateEdges (Node _ []) parents costs = (parents, costs)
+iterateEdges (Node key ((Edge s d w):r)) parents costs  
+    | or [destCost > sourceCost + w, destCost == -1] =
+            iterateEdges (Node key r) (addParent d s parents) (updateCost d (sourceCost + w) costs)
+    | otherwise = iterateEdges (Node key r) parents costs
+
+    where destCost = findCost d costs
+          sourceCost = findCost key costs
+
+setBoolCost :: String -> Bool -> Costs -> Costs
+setBoolCost i b [] = []
+setBoolCost i b (c:r) =  if idf c == i then 
+                            ((Cost i (cost c) b):r)
+                        else
+                            (c:(setBoolCost i b r))
+
+
+isThereEdge :: Graph -> Bool
+isThereEdge [] = False
+isThereEdge ((Node _ []):r) = isThereEdge r
+isThereEdge ((Node _ _):r) = True
